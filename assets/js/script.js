8380e86cd7768b7f9c056b27483d81dc7bbdccbc
Joy Saha <joysaha544@gmail.com>
2021-06-24 12:48:20 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+
+    $('.hf-btn').magnificPopup({
+        type: 'iframe'
+        // other options
+      });
+
+});
